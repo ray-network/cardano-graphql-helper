4cb0e9d7edcba6ceada01410000774c97b0a8d63
Ray Robot <hello@rraayy.com>
2021-04-18 19:57:41 +0300

    update

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 'use strict';
 require('dotenv').config('')
 const express = require('express')
+const cors = require('cors')
 const mountRoutes = require('./routes')
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
+const allowedOrigins = [
+  'https://raywallet.io',
+  'http://localhost:3000',
+]
 
 // App
 const app = express();
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true)
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg = 'The CORS policy for this site does not ' +
+        'allow access from the specified Origin.'
+      return callback(new Error(msg), false)
+    }
+    return callback(null, true)
+  }
+}))
+
 mountRoutes(app)
 
 app.listen(PORT, HOST);
