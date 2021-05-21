dbc08b130eff9720fa28c4d113d06cfdfdc40769
Ray Robot <hello@rraayy.com>
2021-05-21 21:32:04 +0300

    api status

--- routes/status.js
+++ routes/status.js
@@ -6,6 +6,7 @@ module.exports = router
 router.get('/', async (req, res) => {
   res.send({
     maintenance: false,
+    message: 'Wallet node is under maintenance. Services may not be available.',
     url: 'https://status.rraayy.com',
   })
 })
